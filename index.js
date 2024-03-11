190a571eb68914d13f972efdd79c88f152b940aa
Bhavesh  Kumar <bhavesh.kumar@hertshtengroup.com>
2024-03-11 07:16:21 +0000

    adding changes

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const io = new Server(server);
-
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World - Bhavesh Kumar</h1>");
